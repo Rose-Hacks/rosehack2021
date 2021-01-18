024a5b336d55c1493f2cbcf05706ffbff9676e9a
sbala025 <sbala025@ucr.edu>
2021-01-18 12:51:20 -0800

    after rosehack changes

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -214,7 +214,7 @@ var x = setInterval(function() {
 
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("countdown").innerHTML = "VIRTUAL ROSE HACK HAS BEGUN!";
+        document.getElementById("countdown").innerHTML = "VIRTUAL ROSE HACK HAS ENDED!";
     }
     else if (minutes <= 0 && hours <= 0 && days <= 0) {
 
